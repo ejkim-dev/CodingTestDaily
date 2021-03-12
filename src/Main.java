72e576fcb439336992ed5d7a83b56616ad2f7936
Eunjin <eunjin@ninewatt.com>
2021-03-12 15:50:19 +0900

    ListNode 써보기

--- src/Main.java
+++ src/Main.java
@@ -2,8 +2,37 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Test210311 test210311 = new Test210311();
-        System.out.println(test210311.strStr("mississippi","issi"));
+
+     /*   List Node 사용 */
+        ListNode l1 = new ListNode(4);
+        System.out.println(l1.val);
+        System.out.println(l1.next);
+
+        l1.next = new ListNode(2, l1);
+        System.out.println(l1.val);
+        System.out.println(l1.next.val);
+
+        l1.next = new ListNode(1, l1.next);
+        System.out.println(l1.val);
+        System.out.println(l1.next.val);
+
+        System.out.println("--------------------");
+        System.out.println(l1.val);
+        System.out.println(l1.next.val);
+        System.out.println(l1.next.next.val);
+        System.out.println(l1.next.next.next.val);
+        System.out.println(l1.next.next.next.next.val);
+        System.out.println(l1.next.next.next.next.next.val);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3, l2);
+        l2.next = new ListNode(4, l2);
+
+
+
+
+
+        //System.out.println(test210312.mergeTwoLists(l1, l2).val);
 
     }
 }
--- src/Test210312.java
+++ src/Test210312.java
@@ -1,3 +1,8 @@
+import com.azul.crs.com.fasterxml.jackson.databind.util.LinkedNode;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -11,14 +16,47 @@
 public class Test210312 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
 
+        ListNode result = new ListNode();
 
+        /*while (l1 != null && l2 != null) {
+
+            if (l1.val <= l2.val) {
+                result.next = new ListNode(l1.val);
+                result = result.next;
+                l1 = l1.next;
+            } else {
+                result.next = new ListNode(l2.val);
+                result = result.next;
+                l2 = l2.next;
+            }
+
+        }
+
+        if (l1 != null) {
+            result.next = l1;
+        }
+
+        if (l2 != null) {
+            result.next = l2;
+        }*/
+
+        return l1;
     }
 }
 
- class ListNode {
+class ListNode {
       int val;
-      ListNode next;
+      ListNode next; // int val, ListNode next
+
       ListNode() {}
-      ListNode(int val) { this.val = val; }
-      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- }
+
+      ListNode(int val) {
+          this.val = val;
+      }
+
+      ListNode(int val, ListNode next) {
+          this.val = val;
+          this.next = next;
+      }
+
+}

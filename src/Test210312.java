7599af1a313a14c2d083ed30206e69335bc15263
Eunjin <eunjin@ninewatt.com>
2021-03-12 10:23:52 +0900

    add linked list test

---
+++ src/Test210312.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+public class Test210312 {
+    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+
+
+    }
+}
+
+ class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }

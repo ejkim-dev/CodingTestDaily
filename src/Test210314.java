3c31ec995fc71caee0b776a018c87f1c475bd64e
ejkim <biz.ejkim@gmail.com>
2021-03-14 11:47:58 +0900

    백준 소인수분해 완료

--- src/Test210314.java
+++ src/Test210314.java
@@ -2,7 +2,8 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Test210314 {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input = br.readLine();
@@ -15,4 +16,5 @@ public class Main {
         }
 
     }
+
 }

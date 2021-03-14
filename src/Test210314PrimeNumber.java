e3b549173e126f7270cd4c331473e057924f267e
ejkim <biz.ejkim@gmail.com>
2021-03-14 12:17:08 +0900

    백준 소수 완료

--- src/Test210314PrimeNumber.java
+++ src/Test210314PrimeNumber.java
@@ -3,7 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 
-public class Main {
+public class Test210314PrimeNumber {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String input1 = br.readLine();
